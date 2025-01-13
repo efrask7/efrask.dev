7808168aef46c6418e09a4e0e6231fbcc54a317d
efrask7 <efrainschettini.123@gmail.com>
2025-01-12 22:27:25 -0300

    Visualizador de imagenes

---
+++ src/components/ImgViewer.tsx
@@ -0,0 +1,28 @@
+import { BsX } from "react-icons/bs"
+
+interface IImgViewerProps {
+  opened: boolean
+  img: string
+  onClose: () => void
+}
+
+export default function ImgViewer({ opened, img, onClose }: IImgViewerProps) {
+  return (
+    <div className={`fixed top-0 left-0 flex items-center justify-center size-full bg-gray-900/60 px-6 py-8 z-50 ${opened ? '' : 'hidden'}`}>
+      <button className="absolute top-0 right-0 -translate-x-8 translate-y-4 bg-custom rounded-full p-1 text-xl" onClick={onClose}>
+        <BsX />
+      </button>
+
+      {
+        img && (
+          <img
+            src={img}
+            alt="Img"
+            className="w-full h-auto lg:w-auto lg:h-full rounded-sm"
+          />
+        )
+      }
+
+    </div>
+  )
+}
--- src/components/index/ImagesCarrousel.tsx
+++ src/components/index/ImagesCarrousel.tsx
@@ -2,6 +2,8 @@ import { Swiper, SwiperSlide } from "swiper/react"
 import { EffectCards } from "swiper/modules"
 import 'swiper/css'
 import 'swiper/css/effect-cards'
+import ImgViewer from "../ImgViewer"
+import { useState } from "react"
 
 interface IImagesCarrouselProps {
   path: string
@@ -9,6 +11,19 @@ interface IImagesCarrouselProps {
 }
 
 export default function ImagesCarrousel({ path, images }: IImagesCarrouselProps) {
+
+  const [imgViewerProps, setImgViewerProps] = useState({
+    open: false,
+    src: ''
+  })
+
+  function handleCloseImgViewerProps() {
+    setImgViewerProps({
+      open: false,
+      src: ''
+    })
+  }
+
   return (
     <div className="w-full p-2 rounded-md overflow-hidden"
       style={{
@@ -28,11 +43,21 @@ export default function ImagesCarrousel({ path, images }: IImagesCarrouselProps)
                 src={`/proyects/${path}/${image}`}
                 alt="asd"
                 className="rounded-md"
+                onClick={() => setImgViewerProps({
+                  src: `/proyects/${path}/${image}`,
+                  open: true
+                })}
               />
             </SwiperSlide>
           ))
         }
       </Swiper>
+
+      <ImgViewer
+        img={imgViewerProps.src}
+        opened={imgViewerProps.open}
+        onClose={handleCloseImgViewerProps}
+      />
     </div>
   )
 }
