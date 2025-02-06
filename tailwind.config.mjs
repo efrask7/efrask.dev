86d8a291cb4378deaf251255b77240746255ac04
efrask7 <efrainschettini.123@gmail.com>
2025-02-06 17:45:11 -0300

    Se agrego la libreria de "typography" para poder usar markdown sin problemas

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	],
 	theme: {
 		extend: {
 			colors: {
@@ -27,5 +29,7 @@ export default {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography'),
+	],
 }
